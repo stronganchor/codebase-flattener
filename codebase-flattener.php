40aca17dd5925cea87c35e6b864749c54ca03be8
stronganchor <77190959+stronganchor@users.noreply.github.com>
2025-09-19 09:03:01 +0300

    Update codebase-flattener.php

--- codebase-flattener.php
+++ codebase-flattener.php
@@ -101,7 +101,7 @@ function cbf_admin_page() {
 
                 <div class="cbf-section">
                     <label>Folders to Ignore:</label>
-                    <input type="text" id="cbf-ignore-dirs" value="node_modules,vendor,dist,build,.git,__pycache__,venv,env,getid3,media,languages,plugin-update-checker" />
+                    <input type="text" id="cbf-ignore-dirs" value="node_modules,vendor,dist,build,.git,__pycache__,venv,env,getid3,media,iso-languages,languages,plugin-update-checker" />
                     <small>Comma-separated list</small>
                 </div>
             </div>
